f5385ff4d3626aa30f09f97398cdbc9f63e448b9
Agus <aguscm23@gmail.com>
2025-03-15 16:56:59 +0100

    Install Vue

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="main-header">
+  </header>
+  <main class="main">
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const baseUrl = 'http://localhost:3000';
+
+const coreApi = axios.create({
+  baseURL: baseUrl,
+});
+
+
+export {
+
+};
