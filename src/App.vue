5cbef8d27ef2d5c93a43f1501956035dc9ca341a
johanBautista <jsbparra@gmail.com>
2025-04-22 21:37:38 +0200

    add dependencies

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,24 @@
 <template>
   <main class="main">
     <h1>Hola</h1>
+    <div class="sidebar">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
   </main>
+  <router-view />
 </template>
 
 <style scoped>
+.sidebar {
+  width: 200px;
+  background-color: #f0f0f0;
+  padding: 1rem;
+  position: fixed;
+  top: 10;
+  display: flex;
+  justify-content: space-around;
+}
 .main {
   flex: 1;
   overflow-y: auto;
